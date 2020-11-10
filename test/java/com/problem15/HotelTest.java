ea96f87b97112b9a8cb299adae5d2bf4a1f190db
Sippora04 <sippora.toppo95@gmail.com>
2020-11-10 08:23:39 +0530

    Added: finding cheapest rated hotel for Regular Customer

--- test/java/com/problem15/HotelTest.java
+++ test/java/com/problem15/HotelTest.java
@@ -2,19 +2,50 @@ package com.problem15;
 
 import static org.junit.Assert.*;
 
-import org.junit.BeforeClass;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class HotelTest {
-	static HotelReservationSystem hotelReservation = null;
-	@BeforeClass
-	 public  static void setUp() {
-		 hotelReservation = new HotelReservationSystem();
-	 }
-
+	
 	@Test
-	public void givenHotel_added_ShouldTrue() {
+	public void whenHotelAdded_ShouldReturnTrue(){
+		HotelReservationSystem hotelReservationSystem = new HotelReservationSystem();
+		assertTrue(true);
+	   }
+	
+	@Test
+	public void toReturnCheapestHotel()
+	{
 		assertTrue(true);
 	}
+	
+	@Test
+	public void WhenHotelsAdded_ShouldReturnCount()
+	{
+		HotelReservationSystem hotelReservation = new HotelReservationSystem();
+		hotelReservation.addHotel();
+		int count=hotelReservation.countNoOfHotels();
+		Assert.assertEquals(3, count);
+	  }
+	
+	@Test
+	public void WhenHotelsAreAdded_ForRewardCustomers_ShouldReturnTheBestRatedHotel()
+	{
+		HotelReservationSystem hotelReservation = new HotelReservationSystem();
+		hotelReservation.addHotel();
+		int index = 2;
+		String result = hotelReservation.getBestRatedHotel();
+		assertTrue(true);
+	  }
+	
+	@Test
+	public void WhenHotelsAreAdded_ForRegularCustomers_ShouldReturnTheBestRatedHotel()
+	{
+		HotelReservationSystem hotelReservation = new HotelReservationSystem();
+		hotelReservation.addHotel();
+		int index = 1;
+		String result = hotelReservation.getBestRatedHotel();
+		assertTrue(true);
+	  }
 
 }
